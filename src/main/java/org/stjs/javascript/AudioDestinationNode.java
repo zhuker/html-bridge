f5232331c7ab264649f83cd08ee8b049a64589b4
zhukovka <helen.zhukova@gmail.com>
2016-12-13 15:41:34 +0200

    web audio api bridge

---
+++ src/main/java/org/stjs/javascript/AudioDestinationNode.java
@@ -0,0 +1,9 @@
+package org.stjs.javascript;
+
+public class AudioDestinationNode extends AudioNode {
+    /**
+     * Is an unsigned long defining the maximum amount of channels that the
+     * physical device can handle.
+     */
+    public long maxChannelCount;
+}
