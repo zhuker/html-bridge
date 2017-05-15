f5dff19c703ab2e70683276994877bbeed14c60a
Oleksandr Chugai <chugai@gmail.com>
2017-05-15 23:14:29 +0300

    5.0.bv37, fairplay stuff

---
+++ src/main/java/org/stjs/javascript/eme/WebkitNeedKeyEvent.java
@@ -0,0 +1,8 @@
+package org.stjs.javascript.eme;
+
+import org.stjs.javascript.typed.Uint8Array;
+import org.w3c.dom.events.Event;
+
+public abstract class WebkitNeedKeyEvent implements Event {
+    public Uint8Array initData;
+}
