a66536e3b60deb3f18bacc143fb39c2c1efd1f5d
Oleksandr Chugai <chugai@gmail.com>
2017-05-23 00:17:08 +0300

    5.0.bv38 fixed static fields

--- src/main/java/org/stjs/javascript/dom/MediaError.java
+++ src/main/java/org/stjs/javascript/dom/MediaError.java
@@ -6,21 +6,24 @@ public class MediaError {
      * The fetching process for the media resource was aborted by the user agent
      * at the user's request.
      */
-    public final int MEDIA_ERR_ABORTED = 1;
+    public static final int MEDIA_ERR_ABORTED = 1;
     /**
      * A network error of some description caused the user agent to stop
      * fetching the media resource, after the resource was established to be
      * usable.
      */
-    public final int MEDIA_ERR_NETWORK = 2;
+    public static final int MEDIA_ERR_NETWORK = 2;
     /**
      * An error of some description occurred while decoding the media resource,
      * after the resource was established to be usable.
      */
-    public final int MEDIA_ERR_DECODE = 3;
+    public static final int MEDIA_ERR_DECODE = 3;
 
     /** The media resource indicated by the src attribute was not suitable. */
-    public final int MEDIA_ERR_SRC_NOT_SUPPORTED = 4;
+    public static final int MEDIA_ERR_SRC_NOT_SUPPORTED = 4;
+
+    /** Media is encrypted. Safari. */
+    public static final int MEDIA_ERR_ENCRYPTED = 5;
 
     public int code;
 
